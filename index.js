a6aa117c7d487b79227a4696e77539a6b27a3012
Muneeb khan <139747778+students44@users.noreply.github.com>
2024-10-09 15:33:10 -0700

    to-do app complete

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
+// Declare variables to store the input and li values
 let inputBox = document.getElementById('input-box');
 let listContainer = document.getElementById('list-container');
 
-// now we will make the button functional 
-function addTask(){
-  if(inputBox.value === ''){
-    alert("you must write something");
-  }
-  else{
-    let li = document.createElement("li");
+// Calling a function to add task
+function addTask() {
+  if (inputBox.value === '') {
+    alert("Please write something in the input field"); // Alert if input is empty
+  } else {
+    let li = document.createElement("li"); // Create a new list item
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+
+    // Create the cross icon to delete task
+    let span = document.createElement('span');
+    span.innerHTML = "\u00d7";
+    span.className = "close"; // Add a class for styling the close button
+    li.appendChild(span); 
   }
+
+  inputBox.value = ""; // Clear the input field after adding a task
+  saveData();//to stored data
 }
+
+// Event listener for the list container
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+     e.target.parentElement.remove();
+     saveData();
+  }
+}, false);
+
+
+// function for saving data
+function saveData(){
+  localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
